f707a77df27e9af316a5385810029a73a0153e7f
Istvan Varadi <Istvan@Amsos-MacBook-Pro.local>
2021-01-08 19:54:46 +0100

    notes

---
+++ build/precache-manifest.46d818b7aff3d19849c3abba6673e176.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3c340a9a4685ea12808a7e66079a6a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b959ee2a6658c0d0299",
+    "url": "/static/js/2.235e61d4.chunk.js"
+  },
+  {
+    "revision": "d86ec9270abba34e88e4",
+    "url": "/static/js/main.841968be.chunk.js"
+  },
+  {
+    "revision": "583e51a7281672351041",
+    "url": "/static/js/runtime-main.38f647cd.js"
+  }
+]);
---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Note(){
+   return <div className="note">
+       <h1 className>And this is the h1</h1>
+       <p>This is The Paragraph</p>
+   </div>
+}
+
+export default Note;
